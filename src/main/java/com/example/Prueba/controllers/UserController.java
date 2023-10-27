985df1cc2f979f686b7a32f901f19d56291a8a1a
unknown <clleidytatina@gmail.com>
2023-10-26 20:59:04 -0500

    branch develop created

--- src/main/java/com/example/Prueba/controllers/UserController.java
+++ src/main/java/com/example/Prueba/controllers/UserController.java
@@ -1,5 +1,35 @@
 package com.example.Prueba.controllers;
 
-public class UserController {
+import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Prueba.models.User;
+import com.example.Prueba.services.UserService;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	@Autowired
+	UserService userService;
+	
+	@GetMapping()
+	@ResponseStatus(HttpStatus.OK)
+	public ArrayList<User> getUser(){
+		return userService.getUser();
+	}
+	
+	@PostMapping()
+	@ResponseStatus(HttpStatus.CREATED)
+	public User save(@RequestBody User user ) {
+		return this.userService.saveUser(user);
+	}
+	
 }
--- src/main/java/com/example/Prueba/repositories/UserRepository.java
+++ src/main/java/com/example/Prueba/repositories/UserRepository.java
@@ -1,5 +1,11 @@
 package com.example.Prueba.repositories;
 
-public class UserRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.Prueba.models.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
 
 }
