4ced8ab08c42427a12cd6933e8b40e30acfe1485
unknown <clleidytatina@gmail.com>
2023-10-27 10:53:16 -0500

    modified repositories User and Bill

--- src/main/java/com/example/Prueba/repositories/BillRepository.java
+++ src/main/java/com/example/Prueba/repositories/BillRepository.java
@@ -1,12 +1,11 @@
 package com.example.Prueba.repositories;
 
-import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.Prueba.models.Bill;
 
 @Repository
-public interface BillRepository extends CrudRepository<Bill, Long> {
+public interface BillRepository extends JpaRepository<Bill, Long> {
 }
--- src/main/java/com/example/Prueba/repositories/UserRepository.java
+++ src/main/java/com/example/Prueba/repositories/UserRepository.java
@@ -1,11 +1,12 @@
 package com.example.Prueba.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.Prueba.models.User;
 
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 	
 }
